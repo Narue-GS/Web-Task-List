9457b2048760551ea5d498bc7e1b4851813e3a0d
Narue-GS <111442063+Narue-GS@users.noreply.github.com>
2022-08-30 08:24:20 -0300

    feat: add and configure the react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
-import React, { useState } from "react"
+import React from "react"
 import "./App.css"
 import Header from "./components/header"
-import TaskList from "./components/tesk-list"
+import Router from "./router"
 const App = () => {
-  const [tasks, setTasks] = useState([
-    {name:"Dominar React", complited:false, id:0},
-  ])
-  const hendleAdd = (taskName) => {
-    let update = [...tasks, {
-      name: taskName,
-      complited: false,
-      id: Math.random(999),
-    }]
-    setTasks(update);
-  }
-  return (
+  return(
     <>
       <Header />
-      <TaskList hendleAdd={hendleAdd} tasks={tasks}/>
+      <Router />
     </>
   )
+  
 }
 
 export default App;
