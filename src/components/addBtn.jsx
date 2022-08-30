1258571480cf414331b77a0749386fab212cee6d
Narue-GS <111442063+Narue-GS@users.noreply.github.com>
2022-08-30 08:20:34 -0300

    refactor: reorganized the component's structure

--- src/components/addBtn.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const AddBtn = ({children, onClick}) => {
-    return(
-    <button className="add-btn" onClick={onClick}>{children}</button>
-    )
-}
-
-export default AddBtn;
